7368235ec519d28c25d6d84babb3dba3adad1bf1
Dirham satria <dirhaam06@gmail.com>
2025-10-06 01:46:31 +0700

    chore(server): redeploy after CORS_ORIGIN/auth route setup

---
+++ api/[[slug]].ts
@@ -0,0 +1,10 @@
+import app from "../src/app";
+
+// Konfigurasi (opsional, boleh dihapus jika tak perlu)
+export const config = {
+  runtime: "nodejs",
+  maxDuration: 60,
+  memory: 1024,
+};
+
+export default app;
