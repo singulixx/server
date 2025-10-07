0c2d69fa8889cbed2c29e57ab572096bf44c5f96
reyprovidence <reydityads.providence@gmail.com>
2025-10-07 14:43:20 +0700

    fix: vercel api routing and structure

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,13 @@ import app from "../src/app.js";
 
 export const config = {
   api: {
-    bodyParser: false
+    bodyParser: false,
   },
   runtime: "nodejs",
   maxDuration: 60,
-  memory: 1024
+  memory: 1024,
 };
 
-export default function handler(req, res) {
+export default function handler(req: any, res: any) {
   return app(req, res);
 }
